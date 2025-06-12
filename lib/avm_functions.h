fd69bef81bab023886ba70f6bb92d19bc1564df0
ArPapar <apatramanis@yahoo.com>
2025-06-12 15:15:02 +0300

    completed assignment execution

--- lib/avm_functions.h
+++ lib/avm_functions.h
@@ -1,4 +1,21 @@
+#include <assert.h>
 #include "../lib/avm_memcell.h"
+
+typedef void (*memclear_func_t) (avm_memcell*);
+
+void memclear_string(avm_memcell* m);
+void memclear_table(avm_memcell* m);
+
+memclear_func_t memclearFuns[] = {
+    0, /*number*/
+    memclear_string,
+    0, /*bool*/
+    memclear_table,
+    0, /*usefunc*/
+    0, /*libfunc*/
+    0, /*nil*/
+    0  /*undef*/
+}
 
 void avm_warning(string format);
 
--- src/avm_functions.cpp
+++ src/avm_functions.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <cstring>
 #include "../lib/avm_functions.h"
+
+void memclear_string(avm_memcell* m) {
+    assert(get<string*>(m->data));
+    delete get<string*>(m->data);
+}
+
+void memclear_table(avm_memcell* m) {
+    assert(get<avm_table*>(m->data));
+    get<avm_table*>(m->data)->avm_decrefcounter();
+}
+
+void avm_memcellclear(avm_memcell* m) {
+    if (m->type != undef_m) {
+        memclear_func_t f = memclearFuncs[m->type];
+        if (f) {
+            (*f) (m);
+        }
+        m->type = undef_m;
+    }
+}
 
 void avm_warning(string format) {
     cerr << format << endl;
@@ -13,6 +34,16 @@ void avm_assign(avm_memcell* lv, avm_memcell* rv) {
         return;
     }
     if (rv->type == undef_m) {
-        avm_warning("")
+        avm_warning("assigning from 'undef' content!");
+    }
+    avm_memcellclear(lv);
+    
+    memcpy(lv, rv, sizeof(avm_memcell));
+
+    if (lv->type == string_m) {
+        lv->data = rv->data;
+    }
+    else if (lv->type == table_m) {
+        get<avm_table*>(lv->data)->avm_tableincrefcounter();
     }
 }
