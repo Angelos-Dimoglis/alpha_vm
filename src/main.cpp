667d22ae81d168bc65a329d88a82114cb495d526
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-06-13 15:59:00 +0300

    file io

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 #include <unistd.h>
 #include <cassert>
 #include <fstream>
+#include <string>
 
 #include "../lib/avm_functions.h"
 #include "../lib/cpu.h"
 
 using namespace std;
+
+extern bool executionFinished;
+
+extern vector<double> numConsts;
+extern vector<string> strConsts;
+extern vector<string> namedLibfuncs;
+extern vector<userfunc*> userFuncs;
 
 void init_vm() {
 }
@@ -16,6 +24,7 @@ void cleanup_vm() {
 
 int main(int argc, char **argv) {
     bool input_file_set = false;
+    string file_name;
 
     char c;
     while ((c = getopt(argc, argv, "i:")) != -1) {
@@ -25,7 +34,7 @@ int main(int argc, char **argv) {
                 if (optarg != nullptr)
                     input_file_set = true;
 
-                // open the file
+                file_name = optarg;
 
                 break;
             default:
@@ -37,9 +46,22 @@ int main(int argc, char **argv) {
     init_vm();
 
     // load binary file locally
+    instruction *code;
 
-    //while (!programEnd)
-        //execute_cycle();
+    if (input_file_set) {
+        string text;
+        ifstream my_stream(file_name);
+        while (getline(my_stream, text))
+            cout << text << endl;
+    } else {
+        cout << "file not set\n";
+        assert(0);
+    }
+
+
+    while (!executionFinished) {
+        ;
+    }
 
     cleanup_vm();
 
