34fe3bc2d60e1e4983c597113c4a96b2a35d17c6
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-06-15 12:47:02 +0300

    empty arg protection in test

--- testing/run_bytecode.sh
+++ testing/run_bytecode.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $1 ]; then
+    echo "give phase number"
+    exit
+fi
+
 for test_file in testing/byte_code/b_"$1"/*.abc; do
     echo "$test_file"
     ./alpha_vm.out -i "$test_file"
