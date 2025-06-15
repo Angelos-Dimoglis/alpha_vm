3abf5cdee5c34a844ed7f2589a1ea74551d5803f
Angelos-Dimoglis <angelos.titos@gmail.com>
2025-06-15 18:54:00 +0300

    show only the basename of the file

--- testing/run_bytecode.sh
+++ testing/run_bytecode.sh
@@ -7,7 +7,7 @@ fi
 
 for test_file in testing/byte_code/b_"$1"/*.abc; do
     echo "-------------------------------------------------------------------"
-    echo "TESTFILE: $test_file"
+    echo "TESTFILE: $(basename "$test_file")"
     echo
     ./alpha_vm.out -i "$test_file"  1>/dev/null
     echo
